298f02ba59b706a9e85c229e09ad70b9b780a03b
Awilum <sergey.romanenko@flextype.org>
2022-01-11 22:21:41 +0300

    Gender 2.1.0

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-import gender from './index.js';
 import test from 'ava';
+import gender from './index.js';
 
 test('gender return type to be string', t => {
-	t.is(typeof gender(), 'string');
+    t.is(typeof gender(), 'string');
 });
 
 test('gender with locale and extra return type to be string', t => {
-	t.is(typeof gender({locale: 'ru_RU', extra: ['gender']}), 'string');
+    t.is(typeof gender({locale: 'ru_RU', extra: ['gender']}), 'string');
 });
 
 test('gender with locale ru_RU includes', t => {
-	t.true(["Женский", "Мужской"].includes(gender({locale: 'ru_RU'})));
-	t.false(["foo", "bar"].includes(gender({locale: 'ru_RU'})));
+    t.true(['Женский', 'Мужской'].includes(gender({locale: 'ru_RU'})));
+    t.false(['foo', 'bar'].includes(gender({locale: 'ru_RU'})));
 });
